b6803c1aa79e354d82aa23c729196b5e0e134fe6
DuskFlare13 <Redstoner113@gmail.com>
2026-05-05 16:06:48 -0300

    wawa

---
+++ obj/Debug/net472/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace BioLib
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "BioLib";
+        public const string PLUGIN_NAME = "BioLib";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
